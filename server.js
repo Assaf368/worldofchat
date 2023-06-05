59153ccb3093f12a9f9d1a8a8494f57b3e13d74f
Assaf368 <assaf3434@gmail.com>
2023-06-05 10:40:15 +0300

    one more

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
-app.options('/api', cors({
+app.options('*', cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST']
   }));
