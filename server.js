dc3c2244573e90f7f763c2024bb2dc5d71f29b54
Assaf368 <assaf3434@gmail.com>
2023-06-05 10:24:44 +0300

    trying solving cors error

--- server.js
+++ server.js
@@ -19,23 +19,15 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUn
 })
 
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://worldofchat.herokuapp.com/");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      );
-      return res.status(200).json({});
-    }
-    next();
-  });
-
-  
+app.use(cors({
+    origin: '*',
+}))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.options('/api/home', cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST']
+  }));
 
 app.use('/uploads',express.static('uploads'));
 
