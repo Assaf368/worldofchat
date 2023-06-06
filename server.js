76ec69b84366c664bb5a7efe4d8c4b7081c9241c
Assaf368 <assaf3434@gmail.com>
2023-06-06 11:03:08 +0300

    header config in server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true, useUn
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 
